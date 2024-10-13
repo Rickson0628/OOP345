59cf31b1410ebe06948875f413478311cb9a2f0d
Rickson Bozar <rbozar@myseneca.ca>
2024-10-12 21:21:41 -0400

    Lab 2- Implementation of Barbarian and CharacterTpl Module

---
+++ Lab 2/Workshop 2/barbarian.h
@@ -0,0 +1,13 @@
+#ifndef SENECA_BARBARIAN_H
+#define SENECA_BARBARIAN_H
+#include "characterTpl.h"
+
+namespace seneca {
+	template <typename T, typename Ability_t, typename Weapon_t>
+	class Barbarian : public CharacterTpl<T> {
+
+
+	};
+
+}
+#endif
